b2f0bf64a273f3e2cf22beaac3f3f9e5e103208d
shahd <you@example.com>
2020-10-26 15:32:06 +0200

    take video from camera and turn to frame

---
+++ Video_frame.py
@@ -0,0 +1,14 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+i=0
+while(True):
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+    cv2.imwrite('kang'+str(i)+'.jpg',frame)
+    i+=1
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
