37c92f7f83fb837ba834ec95e14ec61ab000aaad
D-mahmoud <44436035+D-mahmoud@users.noreply.github.com>
2020-12-21 16:02:03 +0200

    input+output

--- Data_Augmentation/generate_image.py
+++ Data_Augmentation/generate_image.py
@@ -1,6 +1,3 @@
-# USAGE
-# python generate_images.py --image dog.jpg --output generated_dataset/dogs
-# python generate_images.py --image cat.jpg --output generated_dataset/cats
 
 # import the necessary packages
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -8,29 +5,30 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import load_img
 import numpy as np
 import argparse
+from imutils import paths
+import cv2
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-'''
-ap.add_argument("-i", "--image", required=True,
-	help="path to the input image")
-ap.add_argument("-o", "--output", required=True,
-	help="path to output directory to store augmentation examples")
-'''
-ap.add_argument("-t", "--total", type=int, default=20,
-	help="# of training samples to generate")
-args = vars(ap.parse_args())
-datapath="E:/Grad project/Data Augmentation/Data_Augmentation/13.jpg"
-out="E:/Grad project/Data Augmentation/Data_Augmentation/output"
+datapath="/Users/daliamahmoud/Historia/Data_Augmentation/input"
+out="/Users/daliamahmoud/Historia/Data_Augmentation/output"
 # load the input image, convert it to a NumPy array, and then
 # reshape it to have an extra dimension
-print("[INFO] loading example image...")
-image = load_img(datapath)
-image = img_to_array(image)
-image = np.expand_dims(image, axis=0)
 
+print("[INFO] loading images...")
+imagePaths = list(paths.list_images(datapath))
+data=[]
+
+for imagePath in imagePaths:
+    print(imagePath)
+    image = cv2.imread(imagePath)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (224, 224))
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    data.append(image)
+datanp=np.array(data)
 # construct the image generator for data augmentation then
 # initialize the total number of images generated thus far
+print(len(datanp))
 aug = ImageDataGenerator(
 	rotation_range=30,
 	zoom_range=0.15,
@@ -40,18 +38,23 @@ aug = ImageDataGenerator(
 	horizontal_flip=True,
 	fill_mode="nearest")
 total = 0
-
+imageGen=[]
 # construct the actual Python generator
 print("[INFO] generating images...")
-imageGen = aug.flow(image, batch_size=1, save_to_dir=out,
-	save_prefix="image", save_format="jpg")
 
-# loop over examples from our image data augmentation generator
-for image in imageGen:
+# augmentedImages=[]
+for i in range(len(datanp)):
+        print("Generating is running")
+  
+        imageGen= aug.flow(datanp[i], batch_size=1, save_to_dir=out,
+        save_prefix="image", save_format="jpg")
+        for image in imageGen:
 	# increment our counter
-	total += 1
-
+	        total += 1
 	# if we have reached the specified number of examples, break
 	# from the loop
-	if total == args["total"]:
-		break
+	        if (total%6==0):
+				#total = 0
+		        break
+			
+          
