70249cadb6b70331a4e83de0e8df66b15d1ea605
D-mahmoud <44436035+D-mahmoud@users.noreply.github.com>
2020-12-28 18:35:10 +0200

    hog csv

--- ML models/hog.py
+++ ML models/hog.py
@@ -9,34 +9,40 @@ import numpy as np
 import cv2
 import matplotlib.image as mpimg
 
-datapath="/Users/daliamahmoud/Historia/ML models/input"
-out='/Users/daliamahmoud/Historia/ML models/output'+'.jpg'
+datapath="/Users/daliamahmoud/Historia/Data_Augmentation/input"
+
 
 
 imagePaths = list(paths.list_images(datapath))
 
-data=[]
+
+csv_data=[]
 for imagePath in imagePaths:
         print (imagePath)
-        if imagePath.endswith(".jpg"):
+        if imagePath.endswith(".jpg" or ".png"):
                 img = mpimg.imread(imagePath)
                 image_resized = resize(img, (128, 128))
 
 
         fd, hog_image = hog(image_resized, orientations=9, pixels_per_cell=(8, 8),
         cells_per_block=(2, 2), visualize=True, multichannel=True)
-        plt.imshow(hog_image, cmap="gray")
+        
                                 
-        data.append(hog_image)
-datanp=np.array(data)
+        
+        csv_data.append(fd)
+        
+datanp=np.array(csv_data)
+
+
 
 # construct the image generator for data augmentation then
 # initialize the total number of images generated thus far
-print(len(datanp))
+
 
 for i in range(len(datanp)):
-        cv2.imwrite(out, datanp[i])
-        print("Generating is running")
+
+       
+        print(csv_data[i])
 
   
        
